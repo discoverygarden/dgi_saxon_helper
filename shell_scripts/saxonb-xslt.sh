112e2a00f15fd6e0fb278e157a54de2ad803077d
Jordan Dukart <jordan@discoverygarden.ca>
2019-09-19 13:33:48 -0300

    Wrapper shell for diacritic support.

---
+++ shell_scripts/saxonb-xslt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Grab our input variables.
+saxon_executable=$1
+saxon_params=$2
+transform_file=$3
+
+# Escape parameters from the file.
+
+declare -a escaped_array
+while read -u 3 p; do
+  escaped_array+=("$p")
+done
+
+$saxon_executable -versionmsg:off -ext:on "$saxon_params" "$transform_file" "${escaped_array[@]}"
